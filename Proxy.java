caced76d6d0fe0a8ef044a3aee11a6a24213fe4d
Davi Maciel Chamma Santos <37889219+davichamma@users.noreply.github.com>
2019-03-16 20:32:19 -0300

    Add files via upload

---
+++ Proxy.java
@@ -0,0 +1,89 @@
+package trabalhin;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
+import java.net.SocketTimeoutException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Proxy implements Runnable{
+	public static void main(String[] args) {
+		// Cia uma instancia de proxy e começa a escutar na porta desejada
+		Proxy myProxy = new Proxy(Integer.parseInt(args[0]));
+		myProxy.listen();	
+	}
+	private ServerSocket serverSocket;
+	private volatile boolean running = true;
+	static HashMap<String, File> cache;
+	static HashMap<String, String> blockedSites;
+	static ArrayList<Thread> threadsAbertas;
+	
+	public Proxy(int porta) {
+		cache = new HashMap<>();
+		//a fim de identificar as threads criaremos uma array
+		threadsAbertas = new ArrayList<>();
+		new Thread(this).start();
+		try {
+			serverSocket = new ServerSocket(porta);
+			System.out.println("Esperando por cliente na porta "+ porta + "...");
+			running = true;
+		}catch(SocketException e) {
+			System.out.println("Socket Exception ao conectar com o cliente");
+		}catch(SocketTimeoutException to) {
+			System.out.println("Excedido o tempo limite de conexão");
+		}catch(IOException io) {
+			System.out.println("IO exception ao conectar com o cliente");
+		}
+		
+	}
+	//O servicor escuta na porta lida
+	public void listen() {
+		while(running) {
+			try {
+				//cria o socket do servidor
+				Socket sockServ = serverSocket.accept();
+				//cria uma thread para tratar os requisitos desse socket
+				Thread trata = new Thread(new Tratamento(sockServ));
+				threadsAbertas.add(trata);
+				trata.start();
+			}catch(SocketException e) {
+				System.out.println("Servidor fechado");
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
+			
+		}
+	}
+	//Busca um arquivo na cache
+	public static File getCachedPage(String url){
+		return cache.get(url); //url é uma string a qual possui um arquivo na hash
+	}
+	//Adiciona arquivo na cache
+	public static void addCachedPage(String urlString, File fileToCache){
+		cache.put(urlString, fileToCache);
+	}
+	@Override
+	public void run() {
+		Scanner sc = new Scanner(System.in);
+		String comando;
+		comando = sc.nextLine();
+		while(running) {
+			if(comando.equals("close")) {
+				running = false;
+			}
+		}
+		sc.close();
+	}
+}
+
+
+
+
+
---
+++ Tratamento.java
@@ -0,0 +1,57 @@
+package trabalhin;
+import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.SocketTimeoutException;
+import java.net.URL;
+import javax.imageio.ImageIO;
+
+public class Tratamento implements Runnable{
+	Socket clientSocket;
+	BufferedReader proxyToClientBr;
+	BufferedWriter proxyToClientBw;
+	private Thread httpsClientToServer;
+	//se comunica com o cliente
+	public Tratamento(Socket clientSocket){
+		this.clientSocket = clientSocket;
+		try{
+			this.clientSocket.setSoTimeout(2000);
+			proxyToClientBr = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+			proxyToClientBw = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+		} 
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Override
+	public void run(){
+		// Get Request from client
+				String requestString;
+				try{
+					requestString = proxyToClientBr.readLine();
+				} catch (IOException e) {
+					e.printStackTrace();
+					System.out.println("Erro lendo pedido do cliente\n");
+					return;
+				}
+				System.out.println("Pedido recebido do cliente" + requestString);
+				//primeiro temos que tratar a string de pedido, separando a url do resto
+				//caso haja o pedido de página
+				if(requestString.equals("GET")) {
+					//verifique se a página esta na cache
+					
+				}
+	}
+}
